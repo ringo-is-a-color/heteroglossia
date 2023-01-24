086ad5b361d6b0adcdf855a11e357afdeb0cec4e
ringo is a color <115804577+ringo-is-a-color@users.noreply.github.com>
2023-01-24 20:05:39 +0800

    Use IPv6 URL for curl tests

--- misc/curl_test.sh
+++ misc/curl_test.sh
@@ -3,7 +3,7 @@
 set -e
 trap "exit" INT
 
-curl -sSfx http://127.0.0.1:1081 -o /dev/null -U username:password http://example.com
-curl -sSfx http://127.0.0.1:1081 -o /dev/null -U username:password https://example.com
-curl -sSfx http://127.0.0.1:1081 -o /dev/null -U username:password https://example.com
+curl -sSfx http://[::1]:1081 -o /dev/null -U username:password http://example.com
+curl -sSfx http://[::1]:1081 -o /dev/null -U username:password https://example.com
+curl -sSfx http://[::1]:1081 -o /dev/null -U username:password https://example.com
 echo "Success"
