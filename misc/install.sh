a6b0d35b407467cbbc4fd20d2aecac0172d3dc87
ringo is a color <115804577+ringo-is-a-color@users.noreply.github.com>
2023-01-24 15:50:07 +0800

    Don't use `sudo` if the current user is already root

--- misc/install.sh
+++ misc/install.sh
@@ -19,11 +19,19 @@ else
   exit 1
 fi
 
+function runCommandWithRootCheck() {
+  if [[ $EUID == 0 ]]; then
+    "$@"
+  else
+    sudo "$@"
+  fi
+}
+
 mkdir -p hg
 cd hg
 
 version=$(curl -sL https://api.github.com/repos/ringo-is-a-color/heteroglossia/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
-echo "Downloading the latest release version ($version) of heteroglossia..."
+echo "Downloading the latest release version ${version:1} of heteroglossia..."
 curl -OL# "https://github.com/ringo-is-a-color/heteroglossia/releases/download/$version/heteroglossia_${version:1}_${os}_${arch}.tar.gz"
 printf "Downloading the heteroglossia release's sha256sum...\n"
 curl -OL# "https://github.com/ringo-is-a-color/heteroglossia/releases/download/$version/sha256sums.txt"
@@ -36,7 +44,6 @@ mv heteroglossia hg
 read -r -p "Do you want to download the rules' file? [y/N] " yN
 yN=${yN,,}
 if [[ "$yN" =~ ^(y|yes)$ ]]; then
-  mkdir -p data
   echo "Downloading the domain and IP set rules' file 'domain-ip-set-rules.db'..."
   curl -OL# "https://github.com/ringo-is-a-color/domain-ip-set-rules/raw/release/domain-ip-set-rules.db"
   echo "Downloading the domain and IP set rules' sha256sum..."
@@ -51,7 +58,7 @@ if [[ "$yN" =~ ^(y|yes)$ ]]; then
   read -r -p "Please enter your server's domain: " domain
   password=$(openssl rand -hex 16)
   echo "An example of client & server config files are generated."
-  echo "client.conf.json"
+  echo "'$PWD/client.conf.json'"
   tee client.conf.json <<END
 {
   "inbounds" : {
@@ -68,8 +75,8 @@ if [[ "$yN" =~ ^(y|yes)$ ]]; then
   }
 }
 END
-
-  echo "server.conf.json"
+  echo
+  echo "'$PWD/server.conf.json'"
   tee server.conf.json <<END
 {
   "inbounds" : {
@@ -81,53 +88,57 @@ END
   }
 }
 END
+  echo
 fi
 
 if [[ ! -d /run/systemd/system ]]; then
   exit
 fi
 
-read -r -p "Do you want to install client & server's systemd files into /etc/systemd/system/? [y/N] " yN
+read -r -p "Do you want to install client & server's systemd files into '/etc/systemd/system/'? [y/N] " yN
 yN=${yN,,}
 if [[ "$yN" =~ ^(y|yes)$ ]]; then
-  echo "hg-client.service"
-  tee hg-client.service <<END
-[Unit]
-Description=Heteroglossia Client
-Wants=network-online.target
-After=network.target network-online.target
-
-[Service]
-User=$(whoami)
-ExecStart="$PWD/hg" "$PWD/client.conf.json"
-
-[Install]
-WantedBy=multi-user.target
-END
-
-  echo "hg-server.service"
-  tee hg-server.service <<END
-[Unit]
-Description=Heteroglossia Server
+  server_systemd_file_content="[Unit]
+Description=Heteroglossia(hg) Server
 Wants=network-online.target
 After=network.target network-online.target
 
 [Service]
 User=$(whoami)
 AmbientCapabilities=CAP_NET_BIND_SERVICE
-ExecStart="$PWD/hg" "$PWD/server.conf.json"
+ExecStart=\"$PWD/hg\" \"$PWD/server.conf.json\"
 
 [Install]
-WantedBy=multi-user.target
-END
+WantedBy=multi-user.target"
+  echo "'/etc/systemd/system/hg-server.service'"
+  echo "$server_systemd_file_content"
+  runCommandWithRootCheck sh -c "echo '$server_systemd_file_content' > /etc/systemd/system/hg-server.service"
+  echo
 
-  echo "Copying client.conf.json & hg-server.service into /etc/systemd/system/..."
-  sudo mv {hg-client,hg-server}.service /etc/systemd/system/
-  cat <<END >./remove-systemd-services.sh
+  client_systemd_file_content="[Unit]
+Description=Heteroglossia(hg) Client
+Wants=network-online.target
+After=network.target network-online.target
+
+[Service]
+User=$(whoami)
+ExecStart=\"$PWD/hg\" \"$PWD/client.conf.json\"
+
+[Install]
+WantedBy=multi-user.target"
+  echo "'/etc/systemd/system/hg-client.service'"
+  echo "$client_systemd_file_content"
+  runCommandWithRootCheck sh -c "echo '$client_systemd_file_content' > /etc/systemd/system/hg-client.service"
+
+  cat <<END >./remove-client-server-systemd-services.sh
 #!/bin/bash
 
-echo "Removing client.conf.json & hg-server.service from /etc/systemd/system/..."
-sudo rm /etc/systemd/system/{hg-client,hg-server}.service
+echo "Removing hg-server.service & hg-client.service from '/etc/systemd/system/'..."
+runCommandWithRootCheck rm /etc/systemd/system/{hg-client,hg-server}.service
+echo "Success!"
 END
-  chmod +x ./remove-systemd-services.sh
+  chmod +x ./remove-client-server-systemd-services.sh
+  echo
 fi
+
+echo "Success!"
